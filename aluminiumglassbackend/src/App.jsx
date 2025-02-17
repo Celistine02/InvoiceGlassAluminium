3904aec01332f75698d42e0dedadb9dcb4808019
Celistine02 <148200763+Celistine02@users.noreply.github.com>
2025-02-17 12:11:22 +0200

    updates

--- aluminiumglassbackend/src/App.jsx
+++ aluminiumglassbackend/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/auth/login";
-import MainLayout from "./components/layouts/MainLayout";
-import Overview from "./components/Device Management/Overview";
-import AllPhonesMainArea from "./components/Device Management/AllPhonesMainArea";
-import OverviewMainArea from "./components/Device Management/OverviewMainArea";
-import PaymentsMainArea from "./components/Device Management/paymentsMainArea";
+//import MainLayout from "./components/layouts/MainLayout";
+//import Overview from "./components/Device Management/Overview";
+//import AllPhonesMainArea from "./components/Device Management/AllPhonesMainArea";
+//import OverviewMainArea from "./components/Device Management/OverviewMainArea";
+//import PaymentsMainArea from "./components/Device Management/paymentsMainArea";
 
 export default function App() {
   return (
@@ -13,13 +13,7 @@ export default function App() {
         {/* Login Route */}
         <Route index element={<Login />} />
 
-        {/* Main Layout with Nested Routes */}
-        <Route path="/" element={<MainLayout />}>
-          <Route path="dashboard" element={<Overview />} />
-          <Route path="/overview" element={<OverviewMainArea />} />
-          <Route path="/all-phones" element={<AllPhonesMainArea />} />
-          <Route path="/payments" element={<PaymentsMainArea />} />
-        </Route>
+       
       </Routes>
     </Router>
   );
